de70d7d24972d3a1f7a07673fdca875a728f13ea
David Landes <davidkailandes@gmail.com>
2022-09-16 15:51:54 -0400

    version one of refactoring code completed

--- build/index.js
+++ build/index.js
@@ -1,15 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const CsvFileReader_1 = require("./CsvFileReader");
-const Matchresult_1 = require("./Matchresult");
-const reader = new CsvFileReader_1.CsvFileReader("football.csv");
+const MatchReader_1 = require("./MatchReader");
+const MatchResult_1 = require("./MatchResult");
+const reader = new MatchReader_1.MatchReader("football.csv");
 reader.read();
 let manUnitedWins = 0;
 for (let match of reader.data) {
-    if (match[1] === "Man United" && match[5] === Matchresult_1.MatchResult.HomeWin) {
+    if (match[1] === "Man United" && match[5] === MatchResult_1.MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === "Man United" && match[5] === Matchresult_1.MatchResult.AwayWin) {
+    else if (match[2] === "Man United" && match[5] === MatchResult_1.MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
